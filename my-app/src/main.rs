0c47197ddd76de0ec52b676425bfa1cc36596765
Marcus Weiner <marcus.weiner@gmail.com>
2022-01-28 17:41:10 +0100

    Results from Rustlings session

--- my-app/src/main.rs
+++ my-app/src/main.rs
@@ -1,5 +1,13 @@
 use proc_macro::my_first_proc_macro;
 
 fn main() {
-    my_first_proc_macro!();
+    my_first_proc_macro!(prince);
+    my_first_proc_macro!(alex);
+    my_first_proc_macro!(jen);
+    my_first_proc_macro!(nicola);
+    my_first_proc_macro!(dustin);
+    my_first_proc_macro!(marcus);
+    println!("{}", jen());
+    println!("{}", nicola());
+    println!("{}", marcus());
 }
--- proc-macro/src/lib.rs
+++ proc-macro/src/lib.rs
@@ -1,7 +1,18 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro]
-pub fn my_first_proc_macro(_item: TokenStream) -> TokenStream {
-    todo!()
+pub fn my_first_proc_macro(item: TokenStream) -> TokenStream {
+    println!("{:?}", item);
+    let item: proc_macro2::TokenStream = item.into();
+    let name = proc_macro2::Literal::string("Prince");
+    let tree = proc_macro2::TokenTree::Literal(name);
+    let function = quote! {
+        fn #item() -> &'static str {
+            #tree
+        }
+    };
+    println!("{:?}", function);
+    function.into()
 }
